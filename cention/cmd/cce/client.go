2462f77cbef582a956dd44198f53d155b533173d
Sany Liew <sany.liew@cention.se>
2017-01-25 16:35:21 +0800

    Add Concurrency to Request Command

--- cention/cmd/cce/client.go
+++ cention/cmd/cce/client.go
@@ -7,17 +7,24 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/cention-sany/cention-sdk-go/cention"
 )
 
+const maxSafeRq = 50
+
 var (
-	withAttachment  bool
-	token, endpoint string
+	concurrent                 int
+	withAttachment, insaneTest bool
+	token, endpoint            string
+
+	wg sync.WaitGroup
 )
 
 func main() {
+	t := time.Now()
 	if !flag.Parsed() {
 		flag.Parse()
 	}
@@ -26,30 +33,73 @@ func main() {
 		fmt.Println("Please supply both endpoint and token")
 		os.Exit(1)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	totalRq := concurrent
+	if !insaneTest {
+		if totalRq > maxSafeRq {
+			totalRq = maxSafeRq
+		}
+	}
+	ctx, cancel := context.WithTimeout(context.Background(),
+		time.Minute*time.Duration(totalRq))
+	status := make(chan bool)
+	doneReport := make(chan struct{})
 	go func() {
-		m := new(cention.Message)
-		m.Message_id = "msgid_" + strconv.Itoa(int(time.Now().Unix()))
-		m.Name = "Sany Liew"
-		m.From = "sany.liew@test.com.my"
-		m.Subject = "Creating test errand via API"
-		m.Body = fmt.Sprintf("Test message body at %s",
-			time.Now().Format(time.RFC1123Z))
-		if withAttachment {
-			a1, _ := cention.CreateAttachment("tst1.txt", "text/plain",
-				bytes.NewReader([]byte(`Apple Pen Pineapple Pen`)))
-			a2, _ := cention.CreateAttachment("tst2.txt", "text/plain",
-				bytes.NewReader([]byte(`Orange Juice!`)))
-			m.As = []*cention.Attachment{a1, a2}
+		var success, failed int
+		for ok := range status {
+			if ok {
+				success++
+			} else {
+				failed++
+			}
 		}
-		resp, err := cention.CreateErrand(ctx, endpoint, token, m)
-		if err != nil {
-			fmt.Println("ERROR:", err)
-			os.Exit(1)
-		}
-		fmt.Printf("RESPONSE: %v\n", resp)
-		cancel()
+		fmt.Printf("Total #%d. Success #%d. Failed #%d\n", totalRq,
+			success, failed)
+		fmt.Printf("Total time passed: %.3f\n", time.Since(t).Seconds())
+		close(doneReport)
 	}()
+	wg.Add(totalRq)
+	for i := 1; i <= totalRq; i++ {
+		go func(i int) {
+			defer wg.Done()
+			m := new(cention.Message)
+			m.Message_id = fmt.Sprint("msgid_", i, "_",
+				strconv.Itoa(int(time.Now().Unix())))
+			m.Name = "Sany Liew"
+			m.From = fmt.Sprintf("sany.liew.%d@test.com.my", i)
+			m.Subject = fmt.Sprint("Creating test errand via API #", i)
+			m.Body = fmt.Sprintf("#%d Test message body at %s",
+				i, time.Now().Format(time.RFC1123Z))
+			if withAttachment {
+				a1, _ := cention.CreateAttachment(fmt.Sprintf("tst1_%d.txt", i),
+					"text/plain",
+					bytes.NewReader([]byte(`Apple Pen Pineapple Pen`)))
+				a2, _ := cention.CreateAttachment(fmt.Sprintf("tst2_%d.txt", i),
+					"text/plain",
+					bytes.NewReader([]byte(`Orange Juice!`)))
+				m.As = []*cention.Attachment{a1, a2}
+			}
+			resp, err := cention.CreateErrand(ctx, endpoint, token, m)
+			if err != nil {
+				fmt.Println("Rq #:", i, "ERROR:", err)
+				select {
+				case <-ctx.Done():
+				case status <- false:
+				}
+			} else {
+				fmt.Printf("Rq #:%d RESPONSE: %v\n", i, resp)
+				select {
+				case <-ctx.Done():
+				case status <- true:
+				}
+			}
+		}(i)
+	}
+	go func() {
+		wg.Wait()
+		cancel()
+		close(status)
+	}()
+	<-doneReport
 	<-ctx.Done()
 }
 
@@ -57,5 +107,7 @@ func init() {
 	// command line variables
 	flag.StringVar(&token, "t", "", "Authorized token")
 	flag.StringVar(&endpoint, "e", "", "URL endpoint of Cention application")
+	flag.IntVar(&concurrent, "c", 1, "Number of requests concurrently")
 	flag.BoolVar(&withAttachment, "a", false, "Enable attachment sending")
+	flag.BoolVar(&insaneTest, "insane", false, "")
 }
